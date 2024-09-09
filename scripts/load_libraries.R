2615291d401521f4cdebf9b6c5726b7108a91030
JasonCiemielewski <ciemielewski.jason@gmail.com>
2024-09-09 11:44:17 -0400

    added scripts/load_libraries.R

---
+++ scripts/load_libraries.R
@@ -0,0 +1,16 @@
+#Load Libraries BIFX552-24
+#libraries for progress.qmd
+library(devtools)
+library(LearnR)
+
+#load common libraries
+library(dplyr)
+library(ggplot2)
+library(tibble)
+library(tidyr)
+library(tidyselect)
+library(tidyverse)
+library(plyr)
+library(markdown)
+library(quarto)
+library(magrittr)
